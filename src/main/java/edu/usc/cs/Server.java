0e761d88b81c9b49316a5e459f931565b5be0c63
juefeiyan <juefeiya@usc.edu>
2020-03-09 22:25:05 -0700

    no big change

--- src/main/java/edu/usc/cs/Server.java
+++ src/main/java/edu/usc/cs/Server.java
@@ -18,6 +18,7 @@ public class Server {
         System.out.println("where receive \"OK\" from p2, terminate this program\n");
         while (true) {
             Socket socket = null;
+
             try {
                 //创建一个流套接字并将其连接到指定主机上的指定端口号
                 socket = new Socket(IP_ADDR, PORT);
@@ -25,17 +26,21 @@ public class Server {
                 DataInputStream input = new DataInputStream(socket.getInputStream());
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 System.out.print("input: \t");
-                String str = new BufferedReader(new InputStreamReader(System.in)).readLine();
-                out.writeUTF(str);
+                while(true) {
+                    String str = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                    out.writeUTF(str);
 
-                String ret = input.readUTF();
-                System.out.println("message from p2: " + ret);
-                // 如接收到 "OK" 则断开连接
-                if ("OK".equals(ret)) {
-                    System.out.println("close connection with p2");
-                    Thread.sleep(500);
-                    break;
+                    String ret = input.readUTF();
+                    System.out.println("message from p2: " + ret);
+                    if ("OK".equals(ret)) {
+                        System.out.println("close connection with p2");
+                        Thread.sleep(500);
+                        break;
+                    }
                 }
+
+                // 如接收到 "OK" 则断开连接
+
 
                 out.close();
                 input.close();
